47872214b0b92b23b722585343f56747e03fcc62
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2021-11-09 22:53:15 -0500

    lazy push

---
+++ AirSimE2EDeepLearning/TrainModel.py
@@ -0,0 +1,28 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Lambda, Input, concatenate
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import ELU
+from keras.optimizers import Adam, SGD, Adamax, Nadam
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, CSVLogger, EarlyStopping
+import keras.backend as K
+from keras.preprocessing import image
+
+from keras_tqdm import TQDMNotebookCallback
+
+import json
+import os
+import numpy as np
+import pandas as pd
+from Generator import DriveDataGenerator
+from Cooking import checkAndCreateDir
+import h5py
+from PIL import Image, ImageDraw
+import math
+import matplotlib.pyplot as plt
+
+# << The directory containing the cooked data from the previous step >>
+COOKED_DATA_DIR = '../data_cooked/'
+
+# << The directory in which the model output will be placed >>
+MODEL_OUTPUT_DIR = '../model'
