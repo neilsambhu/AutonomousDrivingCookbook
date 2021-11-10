eb055b190d06def661f69f5a22ca96f9c3d43de4
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2021-11-10 15:41:24 -0500

    update trainmodel to compile

--- AirSimE2EDeepLearning/TrainModel.py
+++ AirSimE2EDeepLearning/TrainModel.py
@@ -1,16 +1,34 @@
-# import os
-# os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.5/bin")
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential, Model
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Lambda, Input, concatenate
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Lambda, Input, concatenate
 from tensorflow.keras.layers import BatchNormalization
-from keras.layers.advanced_activations import ELU
+# from keras.layers.advanced_activations import ELU
+from tensorflow.keras.layers import ELU
 from tensorflow.keras.optimizers import Adam, SGD, Adamax, Nadam
-from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, CSVLogger, EarlyStopping
-import keras.backend as K
-from keras.preprocessing import image
+from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, CSVLogger, EarlyStopping
+import tensorflow.keras.backend as K
+from tensorflow.keras.preprocessing import image
 
-from keras_tqdm import TQDMNotebookCallback
+# from keras_tqdm import TQDMNotebookCallback
+# from tqdm.keras import TqdmCallback
+import tensorflow as tf
+import tensorflow_addons as tfa
+tqdm_callback = tfa.callbacks.TQDMProgressBar()
+
+# devices = tf.config.experimental.list_physical_devices('GPU')
+# tf.config.experimental.set_memory_growth(devices[0], True)
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+
+def fix_gpu():
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+
+
+fix_gpu()
 
 import json
 import os
@@ -40,10 +58,85 @@ num_eval_examples = eval_dataset['image'].shape[0]
 num_test_examples = test_dataset['image'].shape[0]
 
 batch_size=32
+# batch_size=1
 
-# data_generator = DriveDataGenerator(rescale=1./255., horizontal_flip=True, brighten_range=[1-0.4,1+0.4])
-data_generator = DriveDataGenerator(rescale=1./255., horizontal_flip=True)
+data_generator = DriveDataGenerator(rescale=1./255., horizontal_flip=True, brighten_range=0.4)
 train_generator = data_generator.flow\
     (train_dataset['image'], train_dataset['previous_state'], train_dataset['label'], batch_size=batch_size, zero_drop_percentage=0.95, roi=[76,135,0,255])
 eval_generator = data_generator.flow\
     (eval_dataset['image'], eval_dataset['previous_state'], eval_dataset['label'], batch_size=batch_size, zero_drop_percentage=0.95, roi=[76,135,0,255])    
+
+def draw_image_with_label(img, label, prediction=None):
+    theta = label * 0.69 #Steering range for the car is +- 40 degrees -> 0.69 radians
+    line_length = 50
+    line_thickness = 3
+    label_line_color = (255, 0, 0)
+    prediction_line_color = (0, 0, 255)
+    pil_image = image.array_to_img(img, K.image_data_format(), scale=True)
+    print('Actual Steering Angle = {0}'.format(label))
+    draw_image = pil_image.copy()
+    image_draw = ImageDraw.Draw(draw_image)
+    first_point = (int(img.shape[1]/2),img.shape[0])
+    second_point = (int((img.shape[1]/2) + (line_length * math.sin(theta))), int(img.shape[0] - (line_length * math.cos(theta))))
+    image_draw.line([first_point, second_point], fill=label_line_color, width=line_thickness)
+    
+    if (prediction is not None):
+        print('Predicted Steering Angle = {0}'.format(prediction))
+        print('L1 Error: {0}'.format(abs(prediction-label)))
+        theta = prediction * 0.69
+        second_point = (int((img.shape[1]/2) + (line_length * math.sin(theta))), int(img.shape[0] - (line_length * math.cos(theta))))
+        image_draw.line([first_point, second_point], fill=prediction_line_color, width=line_thickness)
+    
+    del image_draw
+    plt.imshow(draw_image)
+    plt.show()
+
+[sample_batch_train_data, sample_batch_test_data] = next(train_generator)
+# for i in range(0, 3, 1):
+#     draw_image_with_label(sample_batch_train_data[0][i], sample_batch_test_data[i])
+
+image_input_shape = sample_batch_train_data[0].shape[1:]
+state_input_shape = sample_batch_train_data[1].shape[1:]
+activation = 'relu'
+
+#Create the convolutional stacks
+pic_input = Input(shape=image_input_shape)
+
+img_stack = Conv2D(16, (3, 3), name="convolution0", padding='same', activation=activation)(pic_input)
+img_stack = MaxPooling2D(pool_size=(2,2))(img_stack)
+img_stack = Conv2D(32, (3, 3), activation=activation, padding='same', name='convolution1')(img_stack)
+img_stack = MaxPooling2D(pool_size=(2, 2))(img_stack)
+img_stack = Conv2D(32, (3, 3), activation=activation, padding='same', name='convolution2')(img_stack)
+img_stack = MaxPooling2D(pool_size=(2, 2))(img_stack)
+img_stack = Flatten()(img_stack)
+img_stack = Dropout(0.2)(img_stack)
+
+#Inject the state input
+state_input = Input(shape=state_input_shape)
+merged = concatenate([img_stack, state_input])
+
+# Add a few dense layers to finish the model
+merged = Dense(64, activation=activation, name='dense0')(merged)
+merged = Dropout(0.2)(merged)
+merged = Dense(10, activation=activation, name='dense2')(merged)
+merged = Dropout(0.2)(merged)
+merged = Dense(1, name='output')(merged)
+
+adam = Nadam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+model = Model(inputs=[pic_input, state_input], outputs=merged)
+model.compile(optimizer=adam, loss='mse')
+
+# model.summary()
+
+plateau_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=0.0001, verbose=1)
+checkpoint_filepath = os.path.join(MODEL_OUTPUT_DIR, 'models', '{0}_model.{1}-{2}.h5'.format('model', '{epoch:02d}', '{val_loss:.7f}'))
+checkAndCreateDir(checkpoint_filepath)
+checkpoint_callback = ModelCheckpoint(checkpoint_filepath, save_best_only=True, verbose=1)
+csv_callback = CSVLogger(os.path.join(MODEL_OUTPUT_DIR, 'training_log.csv'))
+early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
+# callbacks=[plateau_callback, csv_callback, checkpoint_callback, early_stopping_callback, TQDMNotebookCallback()]
+# callbacks=[plateau_callback, csv_callback, checkpoint_callback, early_stopping_callback, TqdmCallback()]
+callbacks=[plateau_callback, csv_callback, checkpoint_callback, early_stopping_callback, tqdm_callback]
+
+history = model.fit_generator(train_generator, steps_per_epoch=num_train_examples//batch_size, epochs=1, callbacks=callbacks,\
+                   validation_data=eval_generator, validation_steps=num_eval_examples//batch_size, verbose=2)
