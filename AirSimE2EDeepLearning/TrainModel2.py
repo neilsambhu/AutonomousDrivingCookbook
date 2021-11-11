4a252e99d32b6c6d373f3aae16fa3bcdb4bef183
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2021-11-11 18:30:19 -0500

    Update TrainModel2.py

--- AirSimE2EDeepLearning/TrainModel2.py
+++ AirSimE2EDeepLearning/TrainModel2.py
@@ -1,10 +1,37 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Lambda, Input, concatenate
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import ELU
+from tensorflow.keras.optimizers import Adam, SGD, Adamax, Nadam
+from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, CSVLogger, EarlyStopping
+import tensorflow.keras.backend as K
+from tensorflow.keras.preprocessing import image
+
+# from keras_tqdm import TQDMNotebookCallback
+# from tqdm.keras import TqdmCallback
+# import tensorflow as tf
+# import tensorflow_addons as tfa
+# tqdm_callback = tfa.callbacks.TQDMProgressBar()
+
+import json
 import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+import numpy as np
+import pandas as pd
+from Generator import DriveDataGenerator
+from Cooking import checkAndCreateDir
 import h5py
+from PIL import Image, ImageDraw
+import math
+import matplotlib.pyplot as plt
+
 # << The directory containing the cooked data from the previous step >>
-COOKED_DATA_DIR = 'data_cooked/'
+COOKED_DATA_DIR = '../data_cooked/'
 
 # << The directory in which the model output will be placed >>
-MODEL_OUTPUT_DIR = 'model'
+MODEL_OUTPUT_DIR = '../model'
 
 train_dataset = h5py.File(os.path.join(COOKED_DATA_DIR, 'train.h5'), 'r')
 eval_dataset = h5py.File(os.path.join(COOKED_DATA_DIR, 'eval.h5'), 'r')
