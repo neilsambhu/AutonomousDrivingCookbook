167723db75d6b2c3aaef4acd33d3d1c1b54a8025
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2021-11-11 22:03:49 -0500

    Update TestModel.py

--- AirSimE2EDeepLearning/TestModel.py
+++ AirSimE2EDeepLearning/TestModel.py
@@ -3,3 +3,18 @@ import sys
 import numpy as np
 import glob
 import os
+
+if ('../../PythonClient/' not in sys.path):
+    sys.path.insert(0, '../../PythonClient/')
+from AirSimClient import *
+
+# << Set this to the path of the model >>
+# If None, then the model with the lowest validation loss from training will be used
+MODEL_PATH = None
+
+if (MODEL_PATH == None):
+    models = glob.glob('model/models/*.h5') 
+    best_model = max(models, key=os.path.getctime)
+    MODEL_PATH = best_model
+    
+print('Using model {0} for testing.'.format(MODEL_PATH))
